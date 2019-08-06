fe014f5123474417561660bdbb33c5dc5891d3ed
Andrew Peck <anp@ucla.edu>
2019-08-06 15:06:03 -0700

    V20190806

--- Release/release.sh
+++ Release/release.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+DATESHORT=$(date +%Y%m%d)
 RELEASEDIR="ASIAGO-"$(date +%Y%m%d-%H%M%S)
 mkdir $RELEASEDIR
 cd ../Layout && sh zip_artwork.sh && cd -
-cp ../Schematic/oh0.pdf $RELEASEDIR/ME0_ASIAGO_SCHEMATIC.pdf
+cp ../Schematic/oh0.pdf $RELEASEDIR/ME0_ASIAGO_SCHEMATIC_$DATESHORT.pdf
 cp ../Schematic/ME0_ASIAGO_BOM.xlsx $RELEASEDIR/
-cp ../Layout/ME0_ASIAGO_ARTWORK.pdf $RELEASEDIR/
+cp ../Layout/ME0_ASIAGO_ARTWORK.pdf $RELEASEDIR/ME0_ASIAGO_ARTWORK_$DATESHORT.pdf
 cp ../Layout/asiago_artwork.zip $RELEASEDIR/
-zip -r asiago_project.zip ../Layout ../Schematic ../Padstacks ../Footprints && mv asiago_project.zip $RELEASEDIR/
+zip -FS -r asiago_project.zip ../Layout ../Schematic ../Padstacks ../Footprints && mv asiago_project.zip $RELEASEDIR/
 
 cd $RELEASEDIR
 mkdir ODB
 tar xzvf ../../Layout/odbjob.tgz -C ./ODB/
-zip -r asiago_odb.zip ODB
+zip -FS -r asiago_odb.zip ODB
 rm -r ODB
 cd -
 

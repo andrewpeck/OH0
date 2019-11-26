86ea3626feeeaae4232e210c48577e5a3980c3ec
Andrew Peck <anp@ucla.edu>
2019-11-25 17:55:25 -0800

    Update release scripts

--- Release/release.sh
+++ Release/release.sh
@@ -4,17 +4,17 @@ DATESHORT=$(date +%Y%m%d)
 RELEASEDIR="ASIAGO-"$(date +%Y%m%d-%H%M%S)
 mkdir $RELEASEDIR
 cd ../Layout && sh zip_artwork.sh && cd -
-cp ../Schematic/oh0.pdf $RELEASEDIR/ME0_ASIAGO_SCHEMATIC_$DATESHORT.pdf
-cp ../Layout/place_txt.txt $RELEASEDIR/ME0_ASIAGO_PLACEMENT_$DATESHORT.txt
-cp ../Schematic/ME0_ASIAGO_BOM.xlsx $RELEASEDIR/ME0_ASIAGO_BOM_$DATESHORT.xlsx
-cp ../Layout/ME0_ASIAGO_ARTWORK.pdf $RELEASEDIR/ME0_ASIAGO_ARTWORK_$DATESHORT.pdf
-cp ../Layout/asiago_artwork.zip $RELEASEDIR/
-zip -FS -r asiago_project.zip ../Layout ../Schematic ../Padstacks ../Footprints && mv asiago_project.zip $RELEASEDIR/
+cp ../Schematic/oh0.pdf $RELEASEDIR/me0_asiago_schematic_$DATESHORT.pdf
+cp ../Layout/place_txt.txt $RELEASEDIR/me0_asiago_placement_$DATESHORT.txt
+cp ../Schematic/ME0_ASIAGO_BOM.xlsx $RELEASEDIR/me0_asiago_bom_$DATESHORT.xlsx
+cp ../Layout/ME0_ASIAGO_ARTWORK.pdf $RELEASEDIR/me0_asiago_artwork_$DATESHORT.pdf
+cp ../Layout/asiago_artwork.zip $RELEASEDIR/me0_asiago_artwork_$DATESHORT.zip
+zip -FS -r asiago_project.zip ../Layout ../Schematic ../Padstacks ../Footprints && mv asiago_project.zip $RELEASEDIR/me0_asiago_project_$DATESHORT.zip
 
 cd $RELEASEDIR
 mkdir ODB
 tar xzvf ../../Layout/odbjob.tgz -C ./ODB/
-zip -FS -r asiago_odb.zip ODB
+zip -FS -r me0_asiago_odb_$DATESHORT.zip ODB
 rm -r ODB
 cd -
 
